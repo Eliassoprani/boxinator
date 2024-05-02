03d9971922be009beb625ba6060061667e09d7da
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-05-02 15:16:33 +0200

    Added routing

--- boxinator-frontend/src/App.jsx
+++ boxinator-frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
-  
+
 
   return (
     <>
-      
+    <div>Hello from frontend</div>
+      <Routes>
+      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      </Routes>
     </>
   )
 }
 
-export default App
+export { App }
