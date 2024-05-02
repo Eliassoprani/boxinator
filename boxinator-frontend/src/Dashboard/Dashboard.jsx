d5bbefedc30f0dd4d12cd39d811556d189e09e8c
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-05-02 16:02:33 +0200

    Added dashboard

---
+++ boxinator-frontend/src/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import './Dashboard.css'
+
+function Dashboard() {
+
+    return (
+        <>
+            <div className="dashboard">
+                <div>Dashboard</div>
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
