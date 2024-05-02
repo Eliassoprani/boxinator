0dd404089112af473ebff0f67dddc450150d083b
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-05-02 15:40:48 +0200

    Added nav bar

--- boxinator-frontend/src/App.jsx
+++ boxinator-frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from "react-router-dom";
-
+import Nav from './Nav/Nav';
 
 function App() {
 
 
   return (
     <>
-    <div>Hello from frontend</div>
-      <Routes>
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-      </Routes>
+      <div className='app'>
+        <Nav />
+        <Routes>
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </div>
     </>
   )
 }
---
+++ boxinator-frontend/src/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import './Nav.css'
+
+function Nav() {
+
+    return (
+        <>
+            <div className="nav">
+                <h1>Boxinator</h1>
+            </div>
+        </>
+    )
+}
+
+export default Nav
