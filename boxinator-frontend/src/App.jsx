3d1e3ce740ff1ce5cc134ffcb627d31e42247df7
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-05-02 16:02:33 +0200

    Added dashboard

--- boxinator-frontend/src/App.jsx
+++ boxinator-frontend/src/App.jsx
@@ -2,17 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from "react-router-dom";
 import Nav from './Nav/Nav';
+import Dashboard from './Dashboard/Dashboard';
 
 function App() {
-
+  const [loggedIn, setLoggedIn] = useState(true);
 
   return (
     <>
       <div className='app'>
         <Nav />
+        {/* Ej inloggad - visa login sida */}
+
+        {loggedIn && (
         <Routes>
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
+        )}
+
       </div>
     </>
   )
