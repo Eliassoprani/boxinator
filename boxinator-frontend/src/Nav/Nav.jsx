69b5dbd5e9efa9944f76b5fd3729f5ec838fc090
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-05-03 11:16:46 +0200

    Added user context and login/signup request to BE

--- boxinator-frontend/src/Nav/Nav.jsx
+++ boxinator-frontend/src/Nav/Nav.jsx
@@ -1,11 +1,22 @@
 import './Nav.css'
+import { useContext } from 'react'
+import { UserContext } from "../App";
 
 function Nav() {
+    const { user, loggedIn } = useContext(UserContext);
+    const initials = user.firstName.get(0) + user.lastName.get(0);
 
     return (
         <>
             <div className="nav">
-                <h1>Boxinator</h1>
+                <div className='title'>
+                    <h1>Boxinator</h1>
+                </div>
+                <div className='userLogo'>
+                    {loggedIn && (
+                        <p>{initials}</p>
+                    )}
+                </div>
             </div>
         </>
     )
