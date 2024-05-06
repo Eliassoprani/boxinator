833758cf59d8fa442107ceb7f9b44cb8fd5fb606
Elias <Elias@soprani.net>
2024-05-06 11:22:13 +0200

    save before exiting

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -1,27 +1,64 @@
 using backend.Models;
 using backend.Payloads;
 using backend.Repositories;
-
+using System.Web.Http;
+using backend.Enums;
+using backend.DTOs;
+using backend.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using static backend.Payloads.AuthPayload;
 namespace backend.Controllers
 {
     public static class UserApi
     {
         public static void ConfigureUserApi(this WebApplication app)
         {
-            app.MapPost("/Users", CreateUser);
-            app.MapGet("/Users", GetAllUsers);
+            var authGroup = app.MapGroup("authentication");
+            authGroup.MapPost("/login", Login);
+            authGroup.MapPost("/signup", Register);
         }
 
-        private static async Task<IResult> GetAllUsers(IUserRepository UserRepository)
+
+        /// <summary>
+        /// a user that is already in the system can be logged in by entering the right credentials
+        /// </summary>
+        /// <param name="userManager"></param> is the class from Identity that is used to access the users table that was generated
+        /// <param name="tokenService"></param> is the TokenServer class that creates a JWT token easily
+        /// <param name="payload"></param> id the data the user needs to provide
+        /// <returns></returns> 200 if the payload is ok and the user is in the database, 400 if the payload is bad or missing
+        public static async Task<IResult> Login(IUserRepository userRepository, LoginPayload payload)
         {
-            return TypedResults.Ok(await UserRepository.GetAllUsers());
+            return TypedResults.Ok(userRepository.Login(payload));
         }
 
-        private static async Task<IResult> CreateUser(IUserRepository UserRepository, UserPostPayload payload)
+        /// <summary>
+        /// a user is not in the system and need to register
+        /// </summary>
+        /// <param name="userManager"></param> is the class from Identity that is used to access the users table that was generated
+        /// <param name="payload"></param> is the data the user needs to provide
+        /// <returns></returns> 201 created if successfull, 400 if the payload is bad or missing
+        public static async Task<IResult> Register(IUserRepository userRepository, RegisterPayload payload)
         {
-            User? User = await UserRepository.CreateAUser(payload);
+            return TypedResults.Ok(userRepository.CreateAUser(payload));
+        }
 
-            return TypedResults.Created("created", User);
+        public static async Task<IResult> getAllUsers(UserManager<User> userManager)
+        {
+            var users = await userManager.Users.ToListAsync();
+
+            var userDTOs = users.Select(user => new UserDTO(user)).ToList();
+            return TypedResults.Ok(userDTOs);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public static async Task<IResult> deleteUserById(IUserRepository userRepository, string id)
+        {
+            bool userDeleted = await userRepository.DeleteUser(id);
+            if (userDeleted) {
+                return TypedResults.Ok("user has been deleted");
+            }
+            return TypedResults.BadRequest();
         }
     }
 }
