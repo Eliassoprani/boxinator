9a5e1afcf7d8e37af9d29c8e655652c87452d075
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-05-06 15:27:19 +0200

    Added order classes, not working with swagger

--- backend/Program.cs
+++ backend/Program.cs
@@ -116,6 +116,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 app.ConfigureAuthApi();
+app.ConfigureOrderController();
 app.Run();
 
 public partial class Program { } // needed for testing - please ignore
