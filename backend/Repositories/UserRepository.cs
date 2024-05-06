c61a8eeb16c80a758cf8d9b19f78ce1392866ada
Elias <Elias@soprani.net>
2024-05-06 11:22:13 +0200

    save before exiting

--- backend/Repositories/UserRepository.cs
+++ backend/Repositories/UserRepository.cs
@@ -1,26 +1,67 @@
 using backend.Models;
 using backend.Data;
-using Microsoft.EntityFrameworkCore;
 using backend.Payloads;
+using backend.Services;
+using Microsoft.AspNetCore.Identity;
+using static backend.Payloads.AuthPayload;
 using backend.Enums;
-using backend.Security;
 namespace backend.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        private DatabaseContext _databaseContext;
-        public UserRepository(DatabaseContext db)
+         private readonly DatabaseContext _databaseContext;
+        private readonly UserManager<User> _userManager;
+        private readonly TokenService _tokenService;
+
+        public UserRepository(DatabaseContext db, UserManager<User> userManager, TokenService tokenService)
         {
             _databaseContext = db;
+            _userManager = userManager;
+            _tokenService = tokenService;
         }
-        public async Task<User?> CreateAUser(UserPostPayload payload)
+        public async Task<RegisterResPayload?> CreateAUser(RegisterPayload payload)
         {
-            return null;
+            if (payload.Email == null) return null;
+            if (payload.Password == null || payload.Password == "") return null;
+            var result = await _userManager.CreateAsync(
+                new User
+                {
+                    UserName = payload.Username,
+                    Email = payload.Email,
+
+                    Role = UserRoles.User
+                },
+                payload.Password!
+            );
+
+            if (result.Succeeded)
+            {
+                return new RegisterResPayload(payload.Username, payload.Email);
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    // Log or display each error message
+                    Console.WriteLine($"Error: {error.Description}");
+                }
+                return null;
+            }
         }
 
-        public async Task<User?> DeleteUser(string Userid)
+        public async Task<bool> DeleteUser(string Userid)
         {
-            return null;
+            var user = await _userManager.FindByIdAsync(Userid);
+
+            if (user == null) return false;
+
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public async Task<IEnumerable<User>> GetAllUsers()
@@ -30,17 +71,30 @@ namespace backend.Repositories
 
         public async Task<User?> GetUserById(string Userid)
         {
-           return null;
+            return null;
         }
 
         public async Task<User?> UpdateUser(string Userid, string FirstName, string LastName, string Email, int Phone)
         {
-           return null;
+            return null;
         }
 
-        public async Task<User?> Login(string Email, string Password)
+        public async Task<LoginResPayload?> Login(LoginPayload payload)
         {
-            return null;
+            if (payload.Email == null || payload.Email.Length == 0) return null;
+            if (payload.Password == null || payload.Password.Length == 0) return null;
+
+            // find by email
+            var user = await _userManager.FindByEmailAsync(payload.Email);
+            if (user == null) return null;
+
+            var isPassword = await _userManager.CheckPasswordAsync(user, payload.Password);
+            if (!isPassword) return null;
+
+            var token = _tokenService.CreateToken(user);
+
+            return new LoginResPayload(token, user.UserName, user.Email, user.Id, user.Role);
+
         }
     }
 }
