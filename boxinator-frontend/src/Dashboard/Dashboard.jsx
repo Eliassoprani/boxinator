34e4c22b7d9eb805baa0112a15ce50866ab5143f
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-06-03 10:16:05 +0200

    Instructions for guest added

--- boxinator-frontend/src/Dashboard/Dashboard.jsx
+++ boxinator-frontend/src/Dashboard/Dashboard.jsx
@@ -2,9 +2,11 @@ import './Dashboard.css'
 import { useContext } from 'react'
 import { UserContext } from "../App";
 import RoleDashboard from './RoleDashboard/RoleDashboard';
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
     const { user } = useContext(UserContext);
+    const navigate = useNavigate();
 
     return (
         <div className="dashboard">
@@ -19,7 +21,15 @@ function Dashboard() {
             )}
 
             {(user.role !== 0 && user.role !== 1) && (
-                <p>As a guest, you are not logged in but you can still make an order.</p>
+                <>
+                    <p>As a guest you are not logged in, but you can still place orders.</p>
+                    <p>No personal information is stored when you place an order as a guest.</p>
+                    <p>If you want to track your order/s, you can register for an account before or after placing your order/s.</p>
+                    <p>
+                        To place an order, go to
+                        <button className="new-shipment-btn" onClick={() => navigate(`/newshipment`)}> New Shipment.</button>
+                    </p>
+                </>
             )}
 
         </div>
