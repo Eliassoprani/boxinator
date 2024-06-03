cda43963e4fa2ecd7f8890ee3835bcf4fbf8740c
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-06-03 16:22:23 +0200

    Nav bar follows scroll

--- boxinator-frontend/src/Nav/Nav.jsx
+++ boxinator-frontend/src/Nav/Nav.jsx
@@ -13,36 +13,34 @@ function Nav() {
 
         setUser({});
         setLoggedIn(false);
-        
+
         navigate("/");
     }
 
     return (
-        <>
-            <div className='nav'>
-                <div className='title' onClick={loggedIn ? () => navigate('/dashboard') : undefined}>
-                    <h1>Boxinator</h1>
-                </div>
-
-                {loggedIn && (
-                    <>
-                        <div>
-                            <div className="nav-link" onClick={() => navigate('/dashboard')}>Dashboard</div>
-                            <div className="nav-link" onClick={() => navigate('/newshipment')}>New Shipment</div>
-                            <div className="nav-link" onClick={() => navigate('/aboutus')}>About Us</div>
-                        </div>
-
-                        <div>
-                            {(user.role === 0 || user.role === 1) && (
-                                <div className="nav-link" onClick={() => navigate('/profile')}>{user.firstName}</div>
-                            )}
-
-                            <button onClick={logout}>{(user.role === 0 || user.role === 1) ? "Log out" : "Log in / Sign up"}</button>
-                        </div>
-                    </>
-                )}
+        <div className='nav'>
+            <div className='title' onClick={loggedIn ? () => navigate('/dashboard') : undefined}>
+                <h1>Boxinator</h1>
             </div>
-        </>
+
+            {loggedIn && (
+                <>
+                    <div>
+                        <div className="nav-link" onClick={() => navigate('/dashboard')}>Dashboard</div>
+                        <div className="nav-link" onClick={() => navigate('/newshipment')}>New Shipment</div>
+                        <div className="nav-link" onClick={() => navigate('/aboutus')}>About Us</div>
+                    </div>
+
+                    <div>
+                        {(user.role === 0 || user.role === 1) && (
+                            <div className="nav-link" onClick={() => navigate('/profile')}>{user.firstName}</div>
+                        )}
+
+                        <button onClick={logout}>{(user.role === 0 || user.role === 1) ? "Log out" : "Log in / Sign up"}</button>
+                    </div>
+                </>
+            )}
+        </div>
     )
 }
 
